6dd5ff2ca56ebf00c0147791794ec78ee7a1efed
beska0013 <gugushvili.beso0013@gmail.com>
2023-03-02 18:23:24 +0400

    add: inventory model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { InventoryModel } from './models/inventory.model';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    SequelizeModule.forRoot({
+      dialect: process.env.DB_DIALECT as 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      models: [
+        InventoryModel
+      ],
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/models/inventory.model.ts
@@ -0,0 +1,23 @@
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+
+@Table({ tableName: 'inventory_tb' })
+export class InventoryModel extends Model<InventoryModel> {
+  @Column({
+    type: DataType.INTEGER,
+    allowNull: false,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  name: string;
+
+  @Column({ type: DataType.INTEGER, allowNull: false })
+  price: string;
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  location: string;
+
+}
